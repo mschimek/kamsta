50df725aa7d877a6704db904a2495eb9b9c3034b
Matthias Schimek <matthias.schimek@kit.edu>
2023-02-15 15:54:13 +0100

    update description

--- benchmarks/mst.cpp
+++ benchmarks/mst.cpp
@@ -43,25 +43,27 @@ inline hybridMST::benchmarks::CmdParameters read_cmdline(int argc,
   cp.set_description("hybrid mst computation");
   cp.set_author("Matthias Schimek");
   cp.add_stringlist('f', "infile", params.graph_params.infiles,
-                    " infile to use for mst computation");
+                    "path to infile to use for mst computation");
   cp.add_string("outfile", params.outfile,
-                " path to file where measurements should be written");
-  cp.add_string("configfile", params.config_file, " path to config file");
+                "path to file where measurements should be written");
+  cp.add_string("configfile", params.config_file, "path to config file:");
   cp.add_size_t("log_n", params.graph_params.log_n,
-                " log_2 of number of vertices");
-  cp.add_size_t("log_m", params.graph_params.log_m,
-                " log_2 of number of edges");
+                "log_2 of number of vertices");
+  cp.add_size_t("log_m", params.graph_params.log_m, "log_2 of number of edges");
   cp.add_string('a', "algorithm", params.algo_params.algo,
-                " algorithm to use for mst computation");
+                "algorithm to use for mst computation");
   cp.add_string('g', "graphtype", params.graph_params.graphtype,
-                " graph to use as input");
+                "graph to use as input "
+                "<GNM|RMAT|RGG_2D|RGG_3D|RHG|GRID_2D|INFILE_WEIGHTED>");
   cp.add_size_t('i', "iterations", params.iterations,
-                " nb of iterations to run");
+                "number of iterations to run");
   cp.add_size_t('t', "threads", params.threads_per_mpi_process,
-                " nb of threads per mpi process");
+                "number of threads per mpi process. This number must match "
+                "the number of OpenMP threads specificed via OMP_NUM_THREADS");
   cp.add_size_t("filter_threshold", params.algo_params.filter_threshold,
-                " nb of threads per mpi process");
-  cp.add_bool('c', "checks", params.do_check, "do check afterwards");
+                "avg vertex degree for the base case in filter boruvka");
+  cp.add_bool('c', "checks", params.do_check,
+              "check the computed mst sequentially");
   cp.add_double('r', "radius", params.graph_params.r,
                 "radius for random geometric graphs");
   cp.add_double("gamma", params.graph_params.gamma,
@@ -78,32 +80,33 @@ inline hybridMST::benchmarks::CmdParameters read_cmdline(int argc,
                 "maximum weight of edge");
   cp.add_size_t(
       "local_kernelization_level", params.algo_params.local_kernelization_level,
-      "0 = do not use kernelization, 1 = contract local edges and vertice");
+      "0 = do not use kernelization, 1 = contract local edges and vertices");
   cp.add_size_t("compression_level", params.algo_params.compression_level,
                 "0 = do not use compression, 1 = use seven bit encoding");
   cp.add_size_t("weak_scaling_level", weak_scaling_level,
-                "0 - no weak scaling, 1 - weak scaling on worker level (each \
-                used core), 2 - weak scaling on mpi proc level");
+                "set weak scaling level: 0 - no weak scaling, 1 - weak "
+                "scaling on worker level (each "
+                "core used)(D), 2 - weak scaling on mpi proc level");
 
-  cp.add_size_t("debug_level", params.debug_level, "set debug level");
+  cp.add_size_t("debug_level", params.debug_level,
+                "set debug level: 0 - no debug output, 1 - debug output");
 
-  if (!cp.process(argc, argv))
-    ctx.abort("problem reading cmd arguments");
+  if (!cp.process(argc, argv)) ctx.abort("problem reading cmd arguments");
   switch (weak_scaling_level) {
-  case 0:
-    params.graph_params.is_weak_scaled_num_workers = false,
-    params.graph_params.is_weak_scaled_num_mpi_procs = false;
-    break;
-  case 1:
-    params.graph_params.is_weak_scaled_num_workers = true,
-    params.graph_params.is_weak_scaled_num_mpi_procs = false;
-    break;
-  case 2:
-    params.graph_params.is_weak_scaled_num_workers = false,
-    params.graph_params.is_weak_scaled_num_mpi_procs = true;
-    break;
-  default:
-    ctx.abort("problem with weak scaling argument");
+    case 0:
+      params.graph_params.is_weak_scaled_num_workers = false,
+      params.graph_params.is_weak_scaled_num_mpi_procs = false;
+      break;
+    case 1:
+      params.graph_params.is_weak_scaled_num_workers = true,
+      params.graph_params.is_weak_scaled_num_mpi_procs = false;
+      break;
+    case 2:
+      params.graph_params.is_weak_scaled_num_workers = false,
+      params.graph_params.is_weak_scaled_num_mpi_procs = true;
+      break;
+    default:
+      ctx.abort("problem with weak scaling argument");
   }
   ctx.set_threads_per_mpi_process(
       static_cast<int>(params.threads_per_mpi_process));
@@ -114,18 +117,18 @@ inline hybridMST::benchmarks::CmdParameters read_cmdline(int argc,
 }
 
 #ifdef USE_EXPLICIT_INSTANTIATION
-extern template hybridMST::WEdgeList
-hybridMST::boruvka(std::vector<hybridMST::WEdge>,
-                   const hybridMST::AlgorithmConfig<hybridMST::WEdge_4_1,
-                                                    hybridMST::WEdgeId_4_1_7>&);
-extern template hybridMST::WEdgeList
-hybridMST::boruvka(std::vector<hybridMST::WEdge>,
-                   const hybridMST::AlgorithmConfig<hybridMST::WEdge_5_1,
-                                                    hybridMST::WEdgeId_6_1_7>&);
-extern template hybridMST::WEdgeList
-hybridMST::boruvka(std::vector<hybridMST::WEdge>,
-                   const hybridMST::AlgorithmConfig<hybridMST::WEdge_6_1,
-                                                    hybridMST::WEdgeId24>&);
+extern template hybridMST::WEdgeList hybridMST::boruvka(
+    std::vector<hybridMST::WEdge>,
+    const hybridMST::AlgorithmConfig<hybridMST::WEdge_4_1,
+                                     hybridMST::WEdgeId_4_1_7>&);
+extern template hybridMST::WEdgeList hybridMST::boruvka(
+    std::vector<hybridMST::WEdge>,
+    const hybridMST::AlgorithmConfig<hybridMST::WEdge_5_1,
+                                     hybridMST::WEdgeId_6_1_7>&);
+extern template hybridMST::WEdgeList hybridMST::boruvka(
+    std::vector<hybridMST::WEdge>,
+    const hybridMST::AlgorithmConfig<hybridMST::WEdge_6_1,
+                                     hybridMST::WEdgeId24>&);
 extern template hybridMST::WEdgeList hybridMST::boruvka(
     std::vector<hybridMST::WEdge>,
     const hybridMST::AlgorithmConfig<hybridMST::WEdge, hybridMST::WEdgeId>&);
@@ -150,14 +153,13 @@ extern template hybridMST::WEdgeList hybridMST::filter_boruvka(
 template <typename InputEdges, typename WEdgeType, typename WEdgeIdType>
 void run_experiments(InputEdges input_edges, hybridMST::VertexRange range,
                      const hybridMST::benchmarks::CmdParameters& params) {
-
   static_assert(
       std::is_same_v<InputEdges, typename std::vector<hybridMST::WEdge>>);
   hybridMST::mpi::MPIContext ctx;
   using InputWEdgeType = typename InputEdges::value_type;
   std::size_t edge_offset =
-      hybridMST::VID_UNDEFINED; // the edge offset is not really needed here
-                                // as we only use the compre
+      hybridMST::VID_UNDEFINED;  // the edge offset is not really needed here
+                                 // as we only use the compre
   const hybridMST::DifferenceEncodedGraph<InputWEdgeType, hybridMST::WEdgeId>
       compressed_input_graph(input_edges, ctx.threads_per_mpi_process(),
                              edge_offset, range);
@@ -201,18 +203,18 @@ void run_experiments(InputEdges input_edges, hybridMST::VertexRange range,
     REORDERING_BARRIER
 
     switch (algo) {
-    case hybridMST::benchmarks::Algorithm::hybridBoruvka: {
-      mst_edges = hybridMST::boruvka(std::move(input_edges), config);
-      break;
-    }
-    case hybridMST::benchmarks::Algorithm::filter_hybridBoruvka: {
-      mst_edges = hybridMST::filter_boruvka(std::move(input_edges), config);
-      break;
-    }
-    default: {
-      ctx.abort("algorithm not available in the benchmark");
-      break;
-    }
+      case hybridMST::benchmarks::Algorithm::hybridBoruvka: {
+        mst_edges = hybridMST::boruvka(std::move(input_edges), config);
+        break;
+      }
+      case hybridMST::benchmarks::Algorithm::filter_hybridBoruvka: {
+        mst_edges = hybridMST::filter_boruvka(std::move(input_edges), config);
+        break;
+      }
+      default: {
+        ctx.abort("algorithm not available in the benchmark");
+        break;
+      }
     }
     REORDERING_BARRIER
     hybridMST::get_timer().stop("mst_complete", i);
@@ -257,8 +259,7 @@ void run_experiments(InputEdges input_edges, hybridMST::VertexRange range,
       }
     }
 
-    if (is_result_wrong)
-      std::terminate();
+    if (is_result_wrong) std::terminate();
     hybridMST::get_timer().reset();
     hybridMST::get_communication_tracker().reset();
   }
@@ -344,7 +345,6 @@ void run_experiments_main(hybridMST::benchmarks::CmdParameters params) {
 
 void run_experiments_verification(
     hybridMST::benchmarks::CmdParameters& params) {
-
   hybridMST::get_timer().start("gen");
   hybridMST::get_timer().stop("gen");
   hybridMST::mpi::MPIContext ctx;
@@ -368,7 +368,7 @@ int main(int argc, char* argv[]) {
   using namespace hybridMST;
   auto start = std::chrono::steady_clock::now();
   bool is_rank_0 = false;
-  hybridMST::mpi::MPIContext ctx; // calls MPI_Init internally
+  hybridMST::mpi::MPIContext ctx;  // calls MPI_Init internally
   const double start_mpi = MPI_Wtime();
   if (ctx.rank() == 0) {
     is_rank_0 = true;
